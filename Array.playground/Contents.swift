17b05ace898310083c9ea1a478d6727bbd69b6c6
Mishal Islam <mishal.islam01@gmail.com>
2015-05-27 18:37:31 +0100

    array

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var array: [String] = ["hello"]
+
+array += ["Mishal"]
+
+array[1]
+
+array.count
+
+array.capacity
+
+array.endIndex
+
+array.first
+
+array.isEmpty
+
+array.last
+
+array.append("something")
+
+array[0] = "Hello"
+
+array
+
+array.removeLast()
+//alt + click to see documentation
+array
+
+array.removeAtIndex(0)
+array
+
+array.insert("Hello", atIndex: 0)
---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let language = "Swift"
+
+let year = 2015
+//integer
+
+var version: Float = 1.0
+//double = 15 decimal and float = 7 decimal
+//always assume it is a double if float is not declared
+
+var boolean = true
+
+//binary opperators
+
+1 + 1
+
+1 - 1
+
+2 / 1
+
+1 * 2
+
+3 % 2
+
+Double(3) / 2
+Float(3) / 2
+
+var levelScore = 0
+levelScore++
+
+var totalScore = levelScore++
+
+levelScore
+totalScore
+
+totalScore = ++levelScore
+levelScore
+totalScore
+
+let on = true
+let off = !on
