1f1ce75b4f92d5a4f55f32b323367f55afc1b086
Mishal Islam <mishal.islam01@gmail.com>
2015-05-27 17:58:00 +0100

    strings

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,4 +3,26 @@
 import UIKit
 //user interface kit
 
-var str = "Hello, playground"
+var greeting : String = "Hello "
+//variable
+//declaring the type is a string
+//mutable string
+
+let name = "Mishal"
+//constant
+//immutable string
+
+//name = "something"
+//error -> cannot assign to 'let' value 'name
+
+var message = greeting + name
+//concatenation
+
+println("Hello")
+//prints message to console
+println("My name is \(name)")
+//string interpolation
+
+println("List: \n1 \t🍎 \n2 \t🍌")
+//new line in string - \n
+//tab in string - \t
